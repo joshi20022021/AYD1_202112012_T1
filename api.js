91c19c5fabc8ecfb1a2e3e1fa7e6918167b2f0a4
Edgar josias can <3114767160409@ingenieria.usac.edu.gt>
2025-08-16 23:44:15 -0600

    reemplaza canción por álbum favorito

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.json({
     nombre: 'Edgar Josías Cán Ajquejay',
-    'Cancion Favorita': "Sweet Child O' Mine",
+    album_favorito: "Appetite for Destruction" 
   });
 });
 
